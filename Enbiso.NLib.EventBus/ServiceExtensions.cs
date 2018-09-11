c2aa9be4c48b52f666c5c67ba277b9749f7cec34
Faraj Farook <farajfarook@gmail.com>
2018-09-11 20:21:25 +1000

    Adding subscription builder

--- Enbiso.NLib.EventBus/ServiceExtensions.cs
+++ Enbiso.NLib.EventBus/ServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Enbiso.NLib.EventBus
@@ -28,10 +29,59 @@ namespace Enbiso.NLib.EventBus
         /// Use event bus
         /// </summary>
         /// <param name="app"></param>
-        public static void UseEventBus(this IApplicationBuilder app)
+        /// <param name="builder"></param>
+        public static void UseEventBus(this IApplicationBuilder app, Action<SubscriptionBuilder> builder = null)
         {
+            
             var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+            builder?.Invoke(new SubscriptionBuilder(eventBus));
             eventBus.Initialize();
+        }
+    }
+
+    public class SubscriptionBuilder
+    {
+        private readonly IEventBus _bus;
+
+        public SubscriptionBuilder(IEventBus bus)
+        {
+            _bus = bus;
+        }
+
+        /// <summary>
+        /// Subscribe to events
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <typeparam name="TEventHandler"></typeparam>
+        public SubscriptionBuilder Subscribe<TEvent, TEventHandler>()
+            where TEvent : IEvent
+            where TEventHandler : IEventHandler<TEvent>
+        {
+            _bus.Subscribe<TEvent, TEventHandler>();
+            return this;
+        }
+
+        /// <summary>
+        /// Subscribe to events
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>        
+        public SubscriptionBuilder Subscribe<TEvent>()
+            where TEvent : IEvent
+        {
+            _bus.Subscribe<TEvent>();
+            return this;
+        }
+
+        /// <summary>
+        /// Subscribe to events dynamically
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <typeparam name="TEventHandler"></typeparam>
+        public SubscriptionBuilder SubscribeDynamic<TEventHandler>(string eventName)
+            where TEventHandler : IDynamicEventHandler
+        {
+            _bus.SubscribeDynamic<TEventHandler>(eventName);
+            return this;
         }
     }
 }
