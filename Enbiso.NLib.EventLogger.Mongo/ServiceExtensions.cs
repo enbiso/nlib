7a020ef6afe7983172afc483cd8ad55014aa9466
Faraj Farook <farajfarook@gmail.com>
2019-03-11 21:15:54 +1100

    Adding event logger in mongodb

---
+++ Enbiso.NLib.EventLogger.Mongo/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Enbiso.NLib.EventLogger.Mongo
+{
+    public static class ServiceExtensions
+    {
+        public static void AddEventLogger(this IServiceCollection services, string dbname)
+        {
+            services.AddEventLogger();
+            services.AddTransient<IEventLogRepo>(sp => new MongoEventLogRepo(sp.GetService<IMongoClient>(), dbname));
+        }
+    }
+}
