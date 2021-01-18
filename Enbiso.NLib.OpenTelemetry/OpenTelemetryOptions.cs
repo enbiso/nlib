05faa6da5c4a372ddceb077e070c9903b1356c33
Faraj Farook <farajfarook@gmail.com>
2021-01-18 19:25:20 +1100

    Adding open telemetry

---
+++ Enbiso.NLib.OpenTelemetry/OpenTelemetryOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Enbiso.NLib.OpenTelemetry
+{
+    public class OpenTelemetryOptions
+    {
+        public string Endpoint { get; set; }
+        public string ServiceName { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+    }
+}
---
+++ Enbiso.NLib.OpenTelemetry/ServiceExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Enbiso.NLib.OpenTelemetry
+{
+    public static class ServiceExtensions
+    {
+        public static void AddOpenTelemetry(this IServiceCollection services, Action<OpenTelemetryOptions> optBuilder)
+        {
+            services.AddOptions();
+            
+            optBuilder ??= _ => {};
+            services.Configure(optBuilder);
+
+            var opts = new OpenTelemetryOptions();
+            optBuilder.Invoke(opts);
+            if(string.IsNullOrEmpty(opts.Endpoint)) return;
+            
+            services.AddOpenTelemetryTracing(builder => builder
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(opts.ServiceName))
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddOtlpExporter(o =>
+                {
+                    o.Endpoint = opts.Endpoint;
+                    o.Credentials = new SslCredentials();
+                    o.Headers = new Metadata();
+                    foreach (var (key, value) in opts.Headers)
+                        o.Headers.Add(key, value);
+                }));
+        }
+    }
+}
